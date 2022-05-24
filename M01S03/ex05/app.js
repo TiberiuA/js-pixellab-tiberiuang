14c8cdcc8c19124b307cf517d73b4fda500fd561
TiberiuA <tiberiu.anghelina@gmail.com>
2022-05-24 22:12:52 +0300

    curs-24-mai-2022

---
+++ M01S03/ex05/app.js
@@ -0,0 +1,24 @@
+var person = {
+  name: 'Tiberiu',
+  surname: 'Anghelina',
+  age: 34,
+  petOwner: false,
+  skills: ['html', 'javascript', 'css'],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Larryson',
+      age: 30,
+    },
+    {
+      name: 'Stevan',
+      surname: 'Stevanson',
+      age: 46,
+    },
+    {
+      name: 'Carol',
+      surname: 'Carolson',
+      age: 28,
+    },
+  ],
+};
