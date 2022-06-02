8eaff9f3c474f1c66bff4b5266db1fdaa7211aa4
TiberiuA <tiberiu.anghelina@gmail.com>
2022-06-02 22:01:40 +0300

    curs-2-iunie-2022

---
+++ M01S04/ex08/app.js
@@ -0,0 +1,28 @@
+var i = 101;
+
+// while (i <= 100) {
+//   console.log(i);
+
+//   i++;
+// }
+
+console.warn(` Ce se intampla daca i este initializat cu valoarea 101?`);
+while (i <= 100) {
+  console.log(i);
+
+  i++;
+}
+
+// Ce se intampla daca i este intitializat cu valoare 101?
+// while (i < 100) {
+// console.log(i)
+// i++;
+// }
+
+// Folosind keyword-ul break opreste bucla
+// atunci cand numarul este egal cu 50 (dar afisaza-l).
+while (i < 100) {
+  console.log(i);
+
+  i++;
+}
